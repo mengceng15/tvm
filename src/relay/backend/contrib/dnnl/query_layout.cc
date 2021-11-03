d29683b5c50e191ff5971bdd1f6d0fcb14a781a2
crazydemo <452642781@qq.com>
2021-11-03 22:01:06 +0800

    11/3 prepare for pr

--- src/relay/backend/contrib/dnnl/query_layout.cc
+++ src/relay/backend/contrib/dnnl/query_layout.cc
@@ -1,3 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * \file src/relay/backend/contrib/dnnl/query_layout.cc
+ * \brief layout auto-query func.
+ */
+
 #include <tvm/relay/attrs/nn.h>
 #include <tvm/relay/expr_functor.h>
 #include <tvm/relay/transform.h>
@@ -46,7 +70,7 @@ void compute_blocks(dims_t &blocks, const dnnl::memory::desc *md) {
             blocks[bd.inner_idxs[iblk]] *= bd.inner_blks[iblk];
     }
 
-bool has_runtime_strides(const dnnl::memory::desc *md) {
+inline bool has_runtime_strides(const dnnl::memory::desc *md) {
         using format_kind_t = dnnl_format_kind_t;
         const format_kind_t blocked = dnnl_blocked;
         if (!(md->data.format_kind==blocked)) return false;
@@ -56,7 +80,7 @@ bool has_runtime_strides(const dnnl::memory::desc *md) {
     }
 
 template <typename T>
-void swap(T &t1, T &t2) {
+inline void swap(T &t1, T &t2) {
     T tmp(t1);
     t1 = t2;
     t2 = tmp;
