9e56ea7f352a27b8321d53a2fcbf4c8031b93229
mengceng he <mengceng.he@intel.com>
2022-07-19 16:34:28 +0800

    change test

--- tests/python/relay/test_vnni_dense.py
+++ tests/python/relay/test_vnni_dense.py
@@ -14,16 +14,18 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-import tvm
-from tvm import relay
-import tvm.contrib.graph_executor as runtime
-import tvm.testing
-
 import numpy as np
+import tvm
+import tvm.testing
+import tvm.contrib.graph_executor as runtime
+from tvm import relay
+
+
+def avx512_vnni_enabled():
+    return "avx512_vnni" in open("/proc/cpuinfo", "r").read()
+
 
 def test_vnni_int8_dense(M, K, N):
-    target = "llvm -mcpu=cascadelake -model=platinum-8280"
     dev = tvm.cpu()
 
     x = relay.var('data', shape=(M, K), dtype="uint8")
@@ -33,7 +35,7 @@ def test_vnni_int8_dense(M, K, N):
     mod = relay.transform.InferType()(mod)
 
     with tvm.transform.PassContext(opt_level=3):
-        lib = relay.build(mod, target=target)
+        lib = relay.build(mod, target="llvm -mcpu=cascadelake -model=platinum-8280")
 
     a_np = np.random.randint(low=-128, high=127, size=(M, K)).astype("uint8")
     b_np = np.random.randint(low=-128, high=127, size=(N, K)).astype("int8")
@@ -44,14 +46,14 @@ def test_vnni_int8_dense(M, K, N):
     module.set_input('data', a_tvm)
     module.set_input('weight', b_tvm)
     module.run()
-    tvm_ans = module.get_output(0).numpy()
     np_ans = np.matmul(a_np.astype("int32"), b_np.T.astype("int32"))
 
-    tol = {"atol": 0, "rtol": 0}
-    tvm.testing.assert_allclose(tvm_ans, np_ans, **tol)
+    tvm.testing.assert_allclose(module.get_output(0).numpy(), np_ans, atol=0, rtol=0)
+
 
 if __name__ == "__main__":
-    test_vnni_int8_dense(128, 3072, 768)
-    test_vnni_int8_dense(128, 768, 768)
-    test_vnni_int8_dense(128, 768, 3072)
-    test_vnni_int8_dense(1, 64, 128)
+    if avx512_vnni_enabled():
+        test_vnni_int8_dense(128, 3072, 768)
+        test_vnni_int8_dense(128, 768, 768)
+        test_vnni_int8_dense(128, 768, 3072)
+        test_vnni_int8_dense(1, 64, 128)
