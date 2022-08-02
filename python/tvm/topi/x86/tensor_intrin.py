6980712c4734f1ae60053f1f2efd46697e5c68fc
mengceng he <mengceng.he@intel.com>
2022-08-02 11:17:00 +0800

    change for normal avx512

--- python/tvm/topi/x86/tensor_intrin.py
+++ python/tvm/topi/x86/tensor_intrin.py
@@ -320,25 +320,31 @@ def dot_16x1x16_uint8_int8_int32_cascadelake():
                         vec_ai32,
                         vec_bi32,
                     )
-            # else:  # Fall back to the normal AVX512
-            #     vec_a = tvm.tir.call_intrin("int8x64", "tir.reinterpret", vec_ai32)
-            #     vec_one = tvm.tir.const(1, "int16x32")
-            #     pair_reduction = tvm.tir.call_llvm_pure_intrin(
-            #         "int16x32",
-            #         "llvm.x86.avx512.pmaddubs.w.512",
-            #         tvm.tir.const(0, "uint32"),
-            #         vec_a,
-            #         vec_b,
-            #     )
-            #     quad_reduction = tvm.tir.call_llvm_pure_intrin(
-            #         "int32x16",
-            #         "llvm.x86.avx512.pmaddw.d.512",
-            #         tvm.tir.const(0, "uint32"),
-            #         pair_reduction,
-            #         vec_one,
-            #     )
-
                     ib.emit(outs[0].vstore(i * 16, quad_reduction))
+            else:  # Fall back to the normal AVX512
+                vec_a = tvm.tir.call_intrin("int8x64", "tir.reinterpret", vec_ai32)
+                vec_one = tvm.tir.const(1, "int16x32")
+                for i in range(4):
+                    vec_b = ins[1].vload([i * 16, 0], "int8x64")
+                    pair_reduction = tvm.tir.call_llvm_pure_intrin(
+                        "int16x32",
+                        "llvm.x86.avx512.pmaddubs.w.512",
+                        tvm.tir.const(0, "uint32"),
+                        vec_a,
+                        vec_b,
+                    )
+                    quad_reduction = tvm.tir.call_llvm_pure_intrin(
+                        "int32x16",
+                        "llvm.x86.avx512.pmaddw.d.512",
+                        tvm.tir.const(0, "uint32"),
+                        pair_reduction,
+                        vec_one,
+                    )
+                    if index == 0:
+                        ib.emit(outs[0].vstore(i * 16, quad_reduction))
+                    else:
+                        ib.emit(outs[0].vstore(i * 16, quad_reduction + outs[0].vload([i * 16], "int32x16")))
+                    
             return ib.get()
 
         # body, reset, update
